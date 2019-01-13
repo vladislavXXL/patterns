1da50f56eae1a6784a8d83fabf8a3eeaf4d1c4ce
vladislavXXL <vitya39@gmail.com>
2019-01-13 14:04:20 +0200

    Pattern Decorator.

--- patterns/src/main/java/decorator/BaseDecorator.java
+++
@@ -1,4 +0,0 @@
-package java.decorator;
-
-public class BaseDecorator {
-}
---
+++ patterns/src/main/java/decorator/decorator1/abstracts/CondimentDecorator.java
@@ -0,0 +1,6 @@
+package java.decorator.decorator1.abstracts;
+
+public abstract class CondimentDecorator extends Beverage {
+
+    public abstract String getDescription();
+}

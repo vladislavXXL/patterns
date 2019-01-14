e80a4a0f827ff2a0a9f7472e8af26988432f3883
vladislavXXL <vitya39@gmail.com>
2019-01-14 22:36:50 +0200

    Project updated

---
+++ patterns/src/main/java/decorator/decorator1/Esspresso.java
@@ -0,0 +1,16 @@
+package decorator.decorator1;
+
+import decorator.decorator1.abstracts.Beverage;
+
+public class Esspresso extends Beverage {
+    public static final double COST = 1.99;
+
+    public Esspresso() {
+        super("Esspresso");
+    }
+
+    public double cost() {
+        return COST;
+    }
+
+}
---
+++ patterns/src/main/java/decorator/decorator1/abstracts/Beverage.java
@@ -0,0 +1,19 @@
+package decorator.decorator1.abstracts;
+
+public abstract class Beverage {
+    public String description;
+
+    public Beverage() {
+
+    }
+
+    public Beverage(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public abstract double cost();
+}
